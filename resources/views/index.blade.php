808aaa0763ebd033ff5ca8e1395d8b187908a4d4
Umasoft Inc <adeogunjumat@umasoftinc.com>
2020-04-17 15:26:47 +0100

    addedd taiwindcss and layout.app

---
+++ resources/views/index.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<div>index of movies</div>
+
+@endsection
---
+++ resources/views/show.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
+<div>index of show movies</div>
+
+@endsection
