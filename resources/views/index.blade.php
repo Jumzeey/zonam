9f0eb5f03d553fd367d68b19a2db1fd641bb4d13
Umasoft Inc <adeogunjumat@umasoftinc.com>
2020-05-11 16:11:08 +0100

    fixed livewire issue

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,11 +38,11 @@
             <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-8">
               @foreach ($nowPlaying as $playing)
                     <div class="mt-8">
-                    <a href="{{route('movies.show', $movie['id'])}}">
+                    <a href="{{route('movies.show', $playing['id'])}}">
                         <img src="{{asset('https://image.tmdb.org/t/p/w500/' . $playing['poster_path'])}}" alt="" class="hover:opacity-75 transition ease-in-out duration-150">
                     </a>
                      <div class="mt-2">
-                       <a href="{{route('movies.show', $movie['id'])}}" class="text-lg mt-2 hover:text-grey-300"> {{$playing['title']}} </a>
+                       <a href="{{route('movies.show', $playing['id'])}}" class="text-lg mt-2 hover:text-grey-300"> {{$playing['title']}} </a>
                      </div>
                      <div class="flex items-center text-grey-400 text-sm mt-1">
                         <svg class="fill-current text-orange-500 w-4" viewBox="0 0 24 24"><g data-name="Layer 2"><path d="M17.56 21a1 1 0 01-.46-.11L12 18.22l-5.1 2.67a1 1 0 01-1.45-1.06l1-5.63-4.12-4a1 1 0 01-.25-1 1 1 0 01.81-.68l5.7-.83 2.51-5.13a1 1 0 011.8 0l2.54 5.12 5.7.83a1 1 0 01.81.68 1 1 0 01-.25 1l-4.12 4 1 5.63a1 1 0 01-.4 1 1 1 0 01-.62.18z" data-name="star"/></g></svg>
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -50,7 +50,7 @@
                     </div>
                     @if (count($movie['videos']['results']) > 0)
                         <div class="mt-12">
-                        <a href='https://youtube.com/watch?v={{$movie['videos']['results'][1]['key']}}' class="flex inline-flex items-center rounded font-semibold text-gray-900 bg-orange-500 px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
+                        <a href='https://youtube.com/watch?v={{$movie['videos']['results'][0]['key']}}' class="flex inline-flex items-center rounded font-semibold text-gray-900 bg-orange-500 px-5 py-4 hover:bg-orange-600 transition ease-in-out duration-150">
                              <svg class="w-6 fill-current" viewBox="0 0 24 24"><path d="M0 0h24v24H0z" fill="none"/><path d="M10 16.5l6-4.5-6-4.5v9zM12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8z"/></svg>
                                 <span class="ml-2">Play Trailer</span>
                         </a>
